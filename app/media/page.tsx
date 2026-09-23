8a1214b9d9814dbff6535ff092b32b29f5e5870e
Cody <codyray.fischer@gmail.com>
2026-09-23 13:18:30 -0500

    taking 2 on media box

--- app/media/page.tsx
+++ app/media/page.tsx
@@ -57,7 +57,7 @@ export default function ExtrasPage() {
   style={{ borderRadius: '12px' }}
   src="https://open.spotify.com/embed/show/7HR9XTBY9tbnZWciUOM0NZ?utm_source=generator"
   width="100%"
-  height="352"
+  height="232"
   frameBorder="0"
   allowFullScreen
   allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
